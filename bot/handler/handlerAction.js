d0f4c9f4d8f6827410ddfa8754b76573bac243c4
cursh-Qeeun-chatbot320 <shanjuloverambo@gmail.com>
2025-04-13 21:20:30 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,8 +72,8 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "💔"){
-  if(event.userID == "61569889349452"){
+        if(event.reaction == "🐼"){
+  if(event.userID == "61574735065504"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "👄"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100081491574719","61569889349452"){
+        if(event.reaction == "😡"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100081491574719","61574735065504"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
